0d2273a523030f259016be5e6418dbf04c1af23e
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-04-26 10:10:59 -0600

    Updated user provider problem
    Updated user provider problem, used the auth change function in the view

--- screens/main/mainScreen/index.jsx
+++ screens/main/mainScreen/index.jsx
@@ -1,24 +1,33 @@
+import { onAuthStateChanged } from "firebase/auth";
 import { Center, Flex, ScrollView, Text } from "native-base";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ViewButton from "../../../components/viewButton";
+import { auth } from "../../../config/firebase/firebase";
 import { useSession } from "../../../providers/session";
 import { useUser } from "../../../providers/user";
 
 function MainScreen() {
   const { logout } = useSession();
-  const { user, setUserInfo } = useUser();
+  const [userInfo, setUserInfo] = useState({});
+  // const { user, setUserInfo } = useUser();
 
   const handleLogout = () => {
     logout();
-    setUserInfo({});
   };
-  useEffect(() => {
-    console.log(user.email);
-  }, [user]);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserInfo(user);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
 
   return (
     <Center safeArea flex={1} h={"full"} justifyContent='flex-start'>
-      <Text>{user.email}</Text>
+      <Text>{userInfo.email}</Text>
       <ViewButton action={logout}>Cerrar Sesion</ViewButton>
     </Center>
   );
