54c5f78171b9d61ebc2b2a592ddc91804536ec67
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-05-16 11:29:54 -0600

    Logout button and logo image placed
    Logout button and logo image placed

--- screens/main/mainScreen/index.jsx
+++ screens/main/mainScreen/index.jsx
@@ -1,11 +1,21 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { Center, Flex, ScrollView, Text } from "native-base";
+import {
+  Box,
+  Center,
+  Flex,
+  Image,
+  Pressable,
+  ScrollView,
+  Text,
+} from "native-base";
 import React, { useEffect, useState } from "react";
 import ViewButton from "../../../components/viewButton";
 import { auth } from "../../../config/firebase/firebase";
 import { useSession } from "../../../providers/session";
 import { useUser } from "../../../providers/user";
-
+import { MaterialIcons } from "@expo/vector-icons";
+import { themeColors } from "../../../config/theme";
+import Isotype from "../../../assets/logos/pressura-logo.png";
 function MainScreen() {
   const { logout } = useSession();
   const [userInfo, setUserInfo] = useState({});
@@ -25,8 +35,31 @@ function MainScreen() {
     }
   });
 
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo]);
+
   return (
     <Center safeArea flex={1} h={"full"} justifyContent='flex-start'>
+      <Flex w={"90%"} mt={1} alignItems='flex-end'>
+        <Pressable onPress={logout}>
+          <Box
+            paddingX={3}
+            paddingY={1}
+            bgColor={themeColors.primario}
+            borderRadius={4}
+          >
+            <MaterialIcons name='logout' size={24} color='white' />
+          </Box>
+        </Pressable>
+      </Flex>
+      <Image
+        style={{ height: 100, width: 125 }}
+        alt='logo'
+        source={Isotype}
+        marginTop={8}
+      />
+
       <Text>{userInfo.email}</Text>
       <ViewButton action={logout}>Cerrar Sesion</ViewButton>
     </Center>
