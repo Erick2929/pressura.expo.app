dbdba59d2fd145266d220ff1bff3222a92441e4a
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-05-30 19:55:14 -0600

    Recover password completed
    the recover password function was added to the app

--- navigators/auth/index.jsx
+++ navigators/auth/index.jsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../../screens/auth/login";
+import RecoverPassword from "../../screens/auth/recoverPassword";
 import Signin from "../../screens/auth/signin";
 
 const { Navigator: StackNavigator, Screen: StackScreen } =
@@ -19,6 +20,11 @@ const AuthNavigator = () => {
           options={{ headerShown: false }}
           name='Signin'
           component={Signin}
+        />
+        <StackScreen
+          options={{ headerShown: false }}
+          name='Recover'
+          component={RecoverPassword}
         />
       </StackNavigator>
     </NavigationContainer>
